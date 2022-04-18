b3ec1639786d58d330b87b77564b29147a5f4ca8
Shinnosuke Sawada <6warashi9@gmail.com>
2022-04-19 08:43:23 +0900

    fix: import path

--- octracemsg/cmd/octracemsg/main.go
+++ octracemsg/cmd/octracemsg/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"octracemsg"
-
 	"golang.org/x/tools/go/analysis/unitchecker"
+
+	"github.com/Warashi/tracemsg/octracemsg"
 )
 
 func main() { unitchecker.Main(octracemsg.Analyzer) }
